f87755cf5de12696fb924e7a0435002c463eee90
heitorsDev <heitorhilles@gmail.com>
2026-04-14 07:21:13 -0300

    gate constants

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/gate/GateConstants.java
@@ -0,0 +1,7 @@
+package org.firstinspires.ftc.teamcode.subsystems.gate;
+
+public class GateConstants {
+    public static String hmGate = "gate";
+    public static double openGate = 0;
+    public static double closedGate = 1;
+}
