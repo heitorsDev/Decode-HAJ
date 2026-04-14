85f651284f7be39e75c63e56915da8c3e15608cf
heitorsDev <heitorhilles@gmail.com>
2026-04-13 21:20:06 -0300

    IntakeConstants created

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/intake/IntakeConstants.java
@@ -0,0 +1,14 @@
+package org.firstinspires.ftc.teamcode.subsystems.intake;
+
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+
+public class IntakeConstants {
+    public static String hmIntake1 = "intake1";
+    public static String hmIntake2 = "intake2";
+
+    public static DcMotorSimple.Direction motorsDirection = DcMotorSimple.Direction.REVERSE;
+    public static double intakeSpeed = 0.7;
+    public static double transferSpeed = 1;
+
+}
